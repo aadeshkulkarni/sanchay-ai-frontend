40912759714b6bdcb4e5c807980774bc9629dbd9
Shivansh Shukla <yourdemocracyishacked@gmail.com>
2023-12-31 12:52:04 +0530

    Add projects list and make Table component
    Add projects list to constants file, and use that list in Table component. Use Table component for rendering projects list. Seperate Table component for better readability and code

---
+++ src/Components/Table.js
@@ -0,0 +1,34 @@
+import { projects } from "../constants/projects";
+
+export const Table = () => (
+  <table className="text-gray-700 border-collapse border border-slate-400 w-full">
+    <thead>
+      <tr className="p-2">
+        <th className="border border-slate-300 p-2">Project</th>
+        <th className="border border-slate-300">Video URL</th>
+        <th className="border border-slate-300">Transcription</th>
+        <th className="border border-slate-300">Subtitles</th>
+        <th className="border border-slate-300">Thumbnail</th>
+      </tr>
+    </thead>
+    <tbody className="text-sm">
+      {projects.map(
+        ({
+          projectName,
+          projectLink,
+          transcriptionFile,
+          subtitleFile,
+          thumbailImg,
+        }) => (
+          <tr>
+            <td className="border border-slate-300 p-2">{projectName}</td>
+            <td className="border border-slate-300">{projectLink}</td>
+            <td className="border border-slate-300">{transcriptionFile}</td>
+            <td className="border border-slate-300">{subtitleFile}</td>
+            <td className="border border-slate-300">{thumbailImg}</td>
+          </tr>
+        )
+      )}
+    </tbody>
+  </table>
+);
---
+++ src/Pages/Projects.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Table } from "../Components/Table";
+
+const Projects = () => {
+  return (
+    <div className="flex justify-center items-center mt-12">
+      <div className="w-[600px] min-w-[400px] border border-gray-100 shadow-lg">
+        <Table />
+      </div>
+    </div>
+  );
+};
+export default Projects;
