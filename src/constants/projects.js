bb89d6e9453edbdbf353194dbc08c661a2fcc10f
Shivansh shukla <29782911+theprogrammerinyou@users.noreply.github.com>
2023-12-31 17:13:57 +0530

    Add checkbox selection on click of label (#5)
    * Add projects list and make Table component
    
    Add projects list to constants file, and use that list in Table component. Use Table component for rendering projects list. Seperate Table component for better readability and code
    
    * [improvement]format main.js and add checkbox selection when clicked on label
    
    * [refactor] rollback changes not in scope of this PR, add projects as props in Table component
    
    * [refactor] change projects import in app.js

---
+++ src/constants/projects.js
@@ -0,0 +1,37 @@
+export const projects = [
+    {
+        projectName: "Tech 2024",
+        projectLink: "https://google.com",
+        transcriptionFile: "transcription.txt",
+        subtitleFile: "subtitle.vtt",
+        thumbailImg: "thumbnail.png"
+    },
+    {
+        projectName: "Tech 2024",
+        projectLink: "https://google.com",
+        transcriptionFile: "transcription.txt",
+        subtitleFile: "subtitle.vtt",
+        thumbailImg: "thumbnail.png"
+    },
+    {
+        projectName: "Tech 2024",
+        projectLink: "https://google.com",
+        transcriptionFile: "transcription.txt",
+        subtitleFile: "subtitle.vtt",
+        thumbailImg: "thumbnail.png"
+    },
+    {
+        projectName: "Tech 2024",
+        projectLink: "https://google.com",
+        transcriptionFile: "transcription.txt",
+        subtitleFile: "subtitle.vtt",
+        thumbailImg: "thumbnail.png"
+    },
+    {
+        projectName: "Tech 2024",
+        projectLink: "https://google.com",
+        transcriptionFile: "transcription.txt",
+        subtitleFile: "subtitle.vtt",
+        thumbailImg: "thumbnail.png"
+    }
+]
