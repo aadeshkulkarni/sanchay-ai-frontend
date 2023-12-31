e49e061a3e4f226aa0312d5027ae986bc3d98006
Yash Hegde <yash4success@gmail.com>
2023-12-31 10:07:22 +0530

    after signup re routing added

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const handleLoginSuccess = (credentialResponse) => {
+    const credentialResponseDecoded = jwtDecode(credentialResponse.credential);
+    console.log(credentialResponseDecoded);
+
+    // Redirect to the '/' route after successful login
+    navigate('/');
+  };
+
   return (
     <div>
       
         <GoogleLogin
-          onSuccess={credentialResponse => {
-          const credentialResponseDecoded = jwtDecode(credentialResponse.credential);
-          console.log(credentialResponseDecoded);
-          }}
-          onError={() => {
-            console.log('Login Failed');
-          }}
-        />;
+        onSuccess={handleLoginSuccess}
+        theme="filled_black"
+        text="signup_with"
+        shape="circle"
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -8,11 +8,14 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
     <GoogleOAuthProvider clientId="326832124300-c8m09jd4i65b0f0usfcbu4ucfqgjp99d.apps.googleusercontent.com">
-       <App />
-    </GoogleOAuthProvider>;
-  </React.StrictMode>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>   
+  </GoogleOAuthProvider>,
+   document.getElementById('root')
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
