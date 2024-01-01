0d9bbd5114876c2d5bfaddcef39d89db00439991
Yash Hegde <yash4success@gmail.com>
2024-01-01 20:53:07 +0530

    corrected the routing.But facing issues on conditional rendering of <Main/> component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,69 @@
+/* eslint-disable jsx-a11y/alt-text */
 import Header from "./Components/Header";
 import Login from "./Components/Login";
 import Main from "./Components/Main";
 import Projects from "./Components/Projects";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { useEffect,useState } from 'react';
+import { jwtDecode } from "jwt-decode";
 
+let flag = false;
 const AppLayout = () => {
+
+  const [user, setUser] = useState({});
+  const [userSignedIn, setUserSignedIn] = useState(false);
+
+  function handleCredentialResponse(response) {
+    console.log("encoded JWT ID Token" + response.credential);
+    var userObject = jwtDecode(response.credential);  
+    console.log(userObject);
+    setUser(userObject);
+    setUserSignedIn(true); 
+    document.getElementById("signInDiv").hidden = true;
+  };
+
+  function handleSignOut(event) {
+    setUser({});
+    setUserSignedIn(false); 
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+  function ReturnuserSignedInValue() {
+    flag = userSignedIn
+    return flag;
+  }
+
+  useEffect(() => {
+    /*global google */
+    google.accounts.id.initialize({
+      client_id: "326832124300-c8m09jd4i65b0f0usfcbu4ucfqgjp99d.apps.googleusercontent.com",
+      callback: handleCredentialResponse,
+      auto_select: true,
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme:"outline",size:"large"}
+    );
+
+    google.accounts.id.prompt();
+  },[])
   return (
     <div className="App">
-    <Header />
+      <Header />
+      <div id="signInDiv"></div>
+      {
+        Object.keys(user).length !== 0 && 
+        <button onClick={(e) => handleSignOut()}>Sign Out</button>
+      }
+      
+      {
+        user && 
+        <div>
+            <img src={user.picture}></img>
+            <h3>{user.name}</h3>
+        </div>
+      }
     <Outlet />
   </div>
   )
