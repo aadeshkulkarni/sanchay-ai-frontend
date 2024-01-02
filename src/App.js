5eb23af212b886000126e73f98cab947aa40b69c
Priyanga <144471073+Priyanga-Developer@users.noreply.github.com>
2024-01-02 18:21:45 +0530

    Google Authentication (#6)
    * created login with authentication
    
    * created login component
    
    * created .env file and removed some extra space

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./Components/Header";
 import Login from "./Components/Login";
 import Main from "./Components/Main";
 import Projects from "./Components/Projects";
+import ProtectedRoute from "./auth/ProtectedRoute"
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
@@ -17,21 +18,25 @@ const router = createBrowserRouter([{
   path: "/",
   element: <AppLayout />,
   children: [
-    {
-      path: "/",
-      element: <Main />,
+    {  
+      element: <ProtectedRoute/>,
+        children:[
+        {
+          index:true, 
+          element:<Main/>
+        },
+        {
+          path: "projects",
+          element:<Projects/>,
+        },
+      ]
     },
     {
       path: "/login",
       element: <Login />,
     },
-    {
-      path: "projects",
-      element: <Projects />,
-    },
   ]
 }]);
 
 const App = () => <RouterProvider router={router} />
-
 export default App;
---
+++ src/auth/ProtectedRoute.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Navigate, Outlet} from "react-router-dom"
+import { useGetUserInfo } from '../Hooks/useGetUserInfo';
+
+const ProtectedRoute = () => {
+   /* Getting isAuth from useGetUserInfo custom hook */
+  const { isAuth }=useGetUserInfo();
+  if(!isAuth){
+    return <Navigate to="/Login"/>;
+ }
+ else{
+  return(<Outlet/>)
+ }
+}
+export default ProtectedRoute
